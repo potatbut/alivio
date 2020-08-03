e783ea1c189a017da4593e667694d53b94977c6d
potatbut <55832724+potatbut@users.noreply.github.com>
2020-08-03 18:49:45 +0300

    Header and headline full adaptive now

--- src/app/index.js
+++ src/app/index.js
@@ -1,7 +1,2 @@
 import  $ from 'jquery'
 import "../index.scss"
-
-$( ".menuToggle" ).click(function(){ // задаем функцию при нажатиии на элемент с классом slide-toggle
-    $( ".menu" ).slideToggle();
-});
-
